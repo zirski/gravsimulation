926d31d12b12d880b79872175946f8fc3b4b6176
zirski <tobiasjhammond@gmail.com>
2022-01-14 13:50:57 -0800

    started work on smoothing out end bounce

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 function love.load()
 
   dist = 100     --just some constants for kinematics
-  grav = 15
+  grav = 25
 
   time = {}
     time.start = 0
@@ -12,17 +12,16 @@ function love.load()
   ball = {}
     ball.rad = 20
     ball.coordX = 250
-    --ball.coordYStart = initBallY - ball.rad
-    --ball.coordYCurrent = 0
     ball.coordY = 250
 
   veloc = {}
-    veloc.init = 50
-    --veloc.final = 0
+    veloc.init = 75
     veloc.current = 0
 
   ground = {}
     ground.y = 450
+
+  distValues = {}
 
 end
 
@@ -30,9 +29,19 @@ function love.update(dt)
   veloc.current = veloc.current + grav
   ball.coordY = ball.coordY + (veloc.current * dt)
 
-  if ball.coordY > (ground.y - 1) - ball.rad then
-    veloc.current = veloc.current * -1
+  --time = time + dt
+  --print(time)
+
+  ball.dist = ground.y - (ball.coordY + 20)
+  --print(ball.dist)
+
+  if ball.coordY > ground.y - ball.rad then
+    veloc.current = veloc.current * (-1 * 0.8)
+    ball.coordY = ground.y - ball.rad - 1
+
+
   end
+
 end
 
 
@@ -49,12 +58,17 @@ function love.draw()
   --ball.coordYCurrent = (veloc.init * time.start) + 0.5 * grav * (time.start)^2 -- calculating current position using velocity inital
   --print(ball.coordYCurrent)
   --ball.coordYCurrent = (veloc.current + veloc.init)/2 * time.start
-  print(veloc.current)
+
+  --print(veloc.current)
 
   love.graphics.setLineWidth(2)
   love.graphics.circle("line", ball.coordX, ball.coordY, ball.rad) --change this to relate it to mass
+
   love.graphics.setLineWidth(2)
   love.graphics.line(0, ground.y, 500, ground.y)
+
+  love.graphics.setLineWidth(1)
+  love.graphics.line(0, ball.coordY, 500, ball.coordY)
 
 end
 
