efc3eca36d565df852f575d447a0aa8a0ccc2628
zirski <tobiasjhammond@gmail.com>
2022-01-14 12:30:27 -0800

    Fixed collision detection & optimized

--- main.lua
+++ main.lua
@@ -1,9 +1,7 @@
 function love.load()
 
   dist = 100     --just some constants for kinematics
-  grav = -19.81
-  mass = 20
-  forceGrav = mass * grav
+  grav = 15
 
   time = {}
     time.start = 0
@@ -12,11 +10,11 @@ function love.load()
   initBallY = 450
 
   ball = {}
-    ball.w = 20
-    ball.h = 20
+    ball.rad = 20
     ball.coordX = 250
-    ball.coordYStart = initBallY - ball.h
-    ball.coordYCurrent = 0
+    --ball.coordYStart = initBallY - ball.rad
+    --ball.coordYCurrent = 0
+    ball.coordY = 250
 
   veloc = {}
     veloc.init = 50
@@ -29,27 +27,32 @@ function love.load()
 end
 
 function love.update(dt)
-  --print(time.start)
+  veloc.current = veloc.current + grav
+  ball.coordY = ball.coordY + (veloc.current * dt)
 
+  if ball.coordY > (ground.y - 1) - ball.rad then
+    veloc.current = veloc.current * -1
+  end
 end
 
 
 function love.draw()
+--[[
   if ball.coordYCurrent > 0 then
     veloc.current = veloc.init + (grav * time.start) --calculating velocity at a certain time
   else
     veloc.current = (veloc.current * -1) -- veloc.current * 0.03
     --print("collision")
   end
-
-  time.start = love.timer.getTime() - time.init
+]]--
+  --time.start = love.timer.getTime() - time.init
   --ball.coordYCurrent = (veloc.init * time.start) + 0.5 * grav * (time.start)^2 -- calculating current position using velocity inital
   --print(ball.coordYCurrent)
-  ball.coordYCurrent = (veloc.current + veloc.init)/2 * time.start
+  --ball.coordYCurrent = (veloc.current + veloc.init)/2 * time.start
   print(veloc.current)
 
   love.graphics.setLineWidth(2)
-  love.graphics.ellipse("line", ball.coordX, ball.coordYStart - ball.coordYCurrent, ball.w, ball.h) --change this to relate it to mass
+  love.graphics.circle("line", ball.coordX, ball.coordY, ball.rad) --change this to relate it to mass
   love.graphics.setLineWidth(2)
   love.graphics.line(0, ground.y, 500, ground.y)
 
