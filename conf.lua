6df1a73f89f9ee55957976e90ddb7f344870ca3e
zirski <tobiasjhammond@gmail.com>
2022-01-17 16:10:21 -0800

    changed window to 2000x500

--- conf.lua
+++ conf.lua
@@ -12,7 +12,7 @@ function love.conf(t)
 
     t.window.title = "Untitled"         -- The window title (string)
     t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
-    t.window.width = 500                -- The window width (number)
+    t.window.width = 2000                -- The window width (number)
     t.window.height = 500               -- The window height (number)
     t.window.borderless = false         -- Remove all border visuals from the window (boolean)
     t.window.resizable = true          -- Let the window be user-resizable (boolean)
